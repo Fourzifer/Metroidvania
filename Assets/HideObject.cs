48e81271c5ef4f37994ecfbd48f12492e519b806
Liam Forsberg <fourzifer@gmail.com>
2022-02-20 23:35:39 +0100

    Level progress

---
+++ Assets/HideObject.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObject : MonoBehaviour
+{
+    [SerializeField] private GameObject fog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            fog.SetActive(false);
+        }
+
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            fog.SetActive(true);
+        }
+
+    }
+}
